80d7ef9d9ebc0f0c07855729d2d22b8a06b8307a
Trần Ngọc Hiếu <hieuxeom.media.1@gmail.com>
2025-01-14 22:15:23 +0700

    fix: fix wrapper size

--- src/components/wrapper/wrapper.tsx
+++ src/components/wrapper/wrapper.tsx
@@ -20,11 +20,24 @@ const Wrapper = ({ size, centerX, centerY, orientation, gapSize, children }: Wra
 		"2xl": "gap-16",
 	};
 
-	const WrapperSize = `max-w-${size}`;
+	const WrapperSize: Record<TBaseSize, string> = {
+		xs: "max-w-xs",
+		sm: "max-w-sm",
+		md: "max-w-md",
+		lg: "max-w-lg",
+		xl: "max-w-xl",
+		"2xl": "max-w-2xl",
+		"3xl": "max-w-3xl",
+		"4xl": "max-w-4xl",
+		"5xl": "max-w-5xl",
+		"6xl": "max-w-6xl",
+		"7xl": "max-w-7xl",
+		"8xl": "max-w-8xl",
+	};
 
 	return (
 		<div
-			className={clsx("w-full flex", GapSize[gapSize], WrapperSize, {
+			className={clsx("w-full flex", WrapperSize[size], GapSize[gapSize], {
 				"flex-row": orientation === "horizontal",
 				"flex-col": orientation !== "horizontal",
 				"justify-center": (orientation === "horizontal" && centerX) || (orientation === "vertical" && centerY),
