3ae03d7799c29f2ea01348656dbb635aa28a7250
Trần Ngọc Hiếu <hieuxeom.media.1@gmail.com>
2025-01-15 12:20:06 +0700

    feat: handle paragraph type for Typography

--- src/components/typography/typography.tsx
+++ src/components/typography/typography.tsx
@@ -3,10 +3,11 @@ import clsx from "clsx";
 interface TypographyProps {
 	type: "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "p" | "small" | "large" | "muted" | "tiny";
 	className?: string;
+	isParagraph: boolean;
 	children: React.ReactNode;
 }
 
-const Typography = ({ type, className, children }: TypographyProps) => {
+const Typography = ({ type, className, isParagraph, children }: TypographyProps) => {
 	const TypoType = type !== "large" && type !== "muted" && type !== "tiny" ? type : "p";
 
 	const MapClassNames: Record<TypographyProps["type"], string> = {
@@ -18,16 +19,34 @@ const Typography = ({ type, className, children }: TypographyProps) => {
 		h6: "scroll-m-20 text-base font-semibold tracking-tight",
 		p: "leading-7",
 		small: "text-sm leading-none",
-		large: "text-lg font-semibold",
+		large: "text-lg",
 		muted: "text-muted-base text-sm",
 		tiny: "text-[12px] leading-none",
 	};
+	const MapParagraphClassNames: Record<TypographyProps["type"], string> = {
+		h1: "",
+		h2: "pb-4",
+		h3: "",
+		h4: "",
+		h5: "",
+		h6: "",
+		p: "",
+		small: "",
+		large: "",
+		muted: "",
+		tiny: "",
+	};
 
-	return <TypoType className={clsx(MapClassNames[type], className)}>{children}</TypoType>;
+	return (
+		<TypoType className={clsx(MapClassNames[type], className, isParagraph && MapParagraphClassNames[type])}>
+			{children}
+		</TypoType>
+	);
 };
 
 Typography.defaultProps = {
 	type: "p",
+	isParagraph: false,
 };
 
 export default Typography;
