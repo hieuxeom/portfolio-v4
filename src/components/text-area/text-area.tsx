6e93d7ec32c972e9534cc9eefd0212dc9064787e
Trần Ngọc Hiếu <hieuxeom.media.1@gmail.com>
2025-01-16 20:34:49 +0700

    feat: create TextArea component

---
+++ src/components/text-area/text-area.tsx
@@ -0,0 +1,72 @@
+import clsx from "clsx";
+import { ChangeEvent, ChangeEventHandler, useState } from "react";
+import Typography from "../typography";
+
+interface TextAreaProps {
+	label: string;
+	value: string | number;
+	name: string;
+	rows: number;
+	placeholder?: string;
+	isInvalid?: boolean;
+	errorMessage?: string;
+	disabled?: boolean;
+	onChange?: ChangeEventHandler<HTMLTextAreaElement>;
+}
+
+const TextArea = ({
+	label,
+	value,
+	name,
+	rows,
+	placeholder,
+	isInvalid,
+	errorMessage,
+	disabled,
+	onChange,
+}: TextAreaProps) => {
+	const [isFocus, setIsFocus] = useState<boolean>(false);
+
+	return (
+		<div className="flex flex-col gap-1">
+			<label
+				className={clsx("uppercase font-bold text-sm transition-all duration-300", {
+					"text-dark": value !== "" || isFocus,
+					"text-dark/25": value === "" && !isFocus,
+				})}
+				htmlFor={label}
+			>
+				{label}
+			</label>
+			<textarea
+				className={clsx("min-w-64 border-2 rounded-xl px-4 py-2 outline-none transition-all duration-300", {
+					"text-dark border-dark": value !== "" || isFocus,
+					"text-dark/25 border-dark/25": value === "" && !isFocus,
+				})}
+				onFocus={() => setIsFocus(true)}
+				onBlur={() => setIsFocus(false)}
+				id={label}
+				value={value}
+				name={name}
+				placeholder={placeholder}
+				onChange={onChange}
+				disabled={disabled}
+				rows={rows}
+			/>
+			{isInvalid && (
+				<Typography
+					type={"small"}
+					className="text-danger"
+				>
+					{errorMessage}
+				</Typography>
+			)}
+		</div>
+	);
+};
+
+TextArea.defaultProps = {
+	rows: 3,
+};
+
+export default TextArea;
