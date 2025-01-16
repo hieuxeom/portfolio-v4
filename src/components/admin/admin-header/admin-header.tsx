ed53f4ab339661c9b9d51e4c68f6fe49fcc0c3da
Trần Ngọc Hiếu <hieuxeom.media.1@gmail.com>
2025-01-16 13:12:33 +0700

    feat: create Admin Header component

---
+++ src/components/admin/admin-header/admin-header.tsx
@@ -0,0 +1,44 @@
+import { useNavigate } from "react-router";
+import Button from "../../button";
+import { ButtonProps } from "../../button/button";
+import Typography from "../../typography";
+
+type BackButtonConfig = {
+	text: string;
+	href: string;
+} & Omit<ButtonProps, "children">;
+
+interface AdminHeaderProps {
+	title: string;
+	backButton?: BackButtonConfig;
+}
+
+const AdminHeader = ({ title, backButton }: AdminHeaderProps) => {
+	const navigate = useNavigate();
+
+	return (
+		<div className={"w-full flex items-center justify-between"}>
+			<Typography
+				type={"h1"}
+				className={"text-dark/25"}
+			>
+				{title}
+			</Typography>
+			{backButton && (
+				<Button
+					size={backButton.size}
+					color={backButton.color}
+					startContent={backButton.startContent}
+					endContent={backButton.endContent}
+					onClick={() => navigate(backButton.href)}
+				>
+					{backButton.text}
+				</Button>
+			)}
+		</div>
+	);
+};
+
+AdminHeader.defaultProps = {};
+
+export default AdminHeader;
