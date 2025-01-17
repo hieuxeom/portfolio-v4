4bec060dfdb2c30ae7959a6827d9400d37fd816e
Trần Ngọc Hiếu <hieuxeom.media.1@gmail.com>
2025-01-17 22:10:00 +0700

    chore: rename `img-path` -> `img_name`

--- src/types/certification.d.ts
+++ src/types/certification.d.ts
@@ -3,10 +3,10 @@ export type TCertificate = {
 	title: string;
 	issued_by: string;
 	issued_date: string;
-	img_path: string;
+	img_name: string;
 	created_at: string;
 	updated_at: string;
 	is_deleted: boolean;
 };
 
-export type TNewCertification = Pick<TCertificate, "title" | "issued_by" | "issued_date" | "img_path">;
+export type TNewCertification = Pick<TCertificate, "title" | "issued_by" | "issued_date" | "img_name">;
