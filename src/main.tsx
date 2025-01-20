9db9133795650fe97a823e218732497681550689
Trần Ngọc Hiếu <hieuxeom.media.1@gmail.com>
2025-01-20 21:04:02 +0700

    chore: add lib react-hot-toast

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,26 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import IndexRouter from "./routes/index-router/index-router.tsx";
+import { Toaster } from "react-hot-toast";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
+		<Toaster
+			position="top-center"
+			reverseOrder={false}
+			gutter={8}
+			containerClassName=""
+			containerStyle={{}}
+			toastOptions={{
+				// Define default options
+				className: "",
+				duration: 2000,
+				style: {
+					background: "#363636",
+					color: "#fff",
+				},
+			}}
+		/>
 		<IndexRouter />
 	</StrictMode>
 );
